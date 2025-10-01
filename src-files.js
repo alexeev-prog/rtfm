3a2c3ae0006fbd793f5159b386c03c5e8eb2b904
alexeev-prog <alexeev-prog@users.noreply.github.com>
2025-10-01 02:05:05 +0000

    deploy: fb6542400f239ed992774fddfca6c7215c567716

--- src-files.js
+++ src-files.js
@@ -1,3 +1,2 @@
-var srcIndex = new Map(JSON.parse('[["rtfm",["",[],["main.rs","man_db.rs","trie.rs","tui.rs"]]]]'));
-createSrcSidebar();
-//{"start":36,"fragment_lengths":[59]}
+createSrcSidebar('[["rtfm",["",[],["main.rs","man_db.rs","trie.rs","tui.rs"]]]]');
+//{"start":19,"fragment_lengths":[59]}
--- trait.impl/core/default/trait.Default.js
+++ trait.impl/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["rtfm",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"rtfm/trie/struct.TrieNode.html\" title=\"struct rtfm::trie::TrieNode\">TrieNode</a>"]]]]);
+    var implementors = Object.fromEntries([["rtfm",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"rtfm/trie/struct.TrieNode.html\" title=\"struct rtfm::trie::TrieNode\">TrieNode</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
